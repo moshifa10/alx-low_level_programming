2c64920fb0f879e2aa1e48645a55f7295ccf664f
moshifa10 <moshifanjabulo@gmail.com>
2022-09-08 16:40:30 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
 /**
- * main - Printing C
+ * main - print the string  in the function
  *
- * Return: Always 0 (successful)
+ * Description: using the main function
+ * this programme prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-	puts ( "\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
