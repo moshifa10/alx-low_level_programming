7c440a0889119bfbbe52fee15c7950d822ac98b2
moshifa10 <moshifanjabulo@gmail.com>
2022-09-08 15:45:25 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Printing C
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
