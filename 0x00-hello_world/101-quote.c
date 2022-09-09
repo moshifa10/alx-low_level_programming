06a0519cf982b500c1a4794e9b9f2f150a74347b
moshifa10 <moshifanjabulo@gmail.com>
2022-09-08 18:12:44 -0700

    8. UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.g>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2,"and that piece of arts is userful\" -Dora Korpar,2015-10-19\n",59);
+	return (1);
+}
