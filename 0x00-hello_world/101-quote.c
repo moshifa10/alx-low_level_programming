a1ceaf5ace66269a8d80481f089d4a1adede2c66
moshifa10 <moshifanjabulo@gmail.com>
2022-09-08 18:38:18 -0700

    8. UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include<unistd.g>
+#include <stdio.h> 
 
-/**
- * main - Prints a text
- *
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(2 , "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+  
+
+ /** 
+  *
+  * main - Entry point 
+  * Return: Always 0 (Success) 
+  */ 
+
+ int main(void) 
+{ 
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
 	return (1);
 }
