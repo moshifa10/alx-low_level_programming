a25dfdaa89f104c232c2ebb176bcdd471fa42c5f
moshifa10 <moshifanjabulo@gmail.com>
2022-09-15 16:23:06 -0700

    3. Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,7 +17,6 @@ if (month >= 3 && day >= 60)
 {
 day++;
 }
-
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
@@ -30,7 +29,7 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day - 31 , year);
 else
 {
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 365 -day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
